ba74b7a8c42696c6cde14d9bc933c07ce1d182ea
Martin Tøften <martin@bakkenbaeck.no>
2017-04-25 09:44:02 +0200

    Add logic to PaymentRequestConfirmPresenter

--- app/src/main/java/com/tokenbrowser/presenter/PaymentRequestConfirmPresenter.java
+++ app/src/main/java/com/tokenbrowser/presenter/PaymentRequestConfirmPresenter.java
@@ -17,24 +17,204 @@
 
 package com.tokenbrowser.presenter;
 
+import android.view.View;
+import android.widget.Toast;
+
+import com.tokenbrowser.R;
+import com.tokenbrowser.crypto.util.TypeConverter;
+import com.tokenbrowser.model.local.User;
+import com.tokenbrowser.model.sofa.Payment;
+import com.tokenbrowser.util.EthUtil;
+import com.tokenbrowser.util.ImageUtil;
+import com.tokenbrowser.util.OnSingleClickListener;
+import com.tokenbrowser.util.PaymentType;
+import com.tokenbrowser.view.BaseApplication;
 import com.tokenbrowser.view.fragment.DialogFragment.PaymentConfirmationDialog;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.subscriptions.CompositeSubscription;
 
 public class PaymentRequestConfirmPresenter implements Presenter<PaymentConfirmationDialog> {
 
     private PaymentConfirmationDialog view;
+    private CompositeSubscription subscriptions;
+    private boolean firstTimeAttached = true;
+
+    private User user;
+    private String encodedEthAmount;
+    private String tokenId;
+    private String paymentAddress;
+    private String memo;
+    private @PaymentType.Type int paymentType;
 
     @Override
     public void onViewAttached(PaymentConfirmationDialog view) {
         this.view = view;
+
+        if (this.firstTimeAttached) {
+            this.firstTimeAttached = false;
+            initLongLivingObjects();
+        }
+
+        initShortLivingObjects();
+    }
+
+    private void initLongLivingObjects() {
+        this.subscriptions = new CompositeSubscription();
+    }
+
+    private void initShortLivingObjects() {
+        initClickListeners();
+        processBundleData();
+        updateView();
+        tryLoadUser();
+    }
+
+    private void initClickListeners() {
+        this.view.getBinding().reject.setOnClickListener(__ -> handleRejectClicked());
+        this.view.getBinding().approve.setOnClickListener(new OnSingleClickListener() {
+            @Override
+            public void onSingleClick(View v) {
+                handleApprovedClicked();
+            }
+        });
+    }
+
+    private void handleApprovedClicked() {
+        final Payment payment = new Payment()
+                .setValue(this.encodedEthAmount);
+
+        if (this.paymentAddress != null) {
+            payment.setToAddress(this.paymentAddress);
+            this.view.getPaymentConfirmationListener()
+                    .onExternalPaymentApproved(payment);
+        } else {
+            this.view.getPaymentConfirmationListener()
+                    .onTokenPaymentApproved(this.tokenId, payment);
+        }
+        this.view.dismiss();
+    }
+
+    private void handleRejectClicked() {
+        this.view.getPaymentConfirmationListener().onPaymentRejected();
+        this.view.dismiss();
+    }
+
+    @SuppressWarnings("WrongConstant")
+    private void processBundleData() {
+        this.tokenId = this.view.getArguments().getString(PaymentConfirmationDialog.TOKEN_ID);
+        this.paymentAddress = this.view.getArguments().getString(PaymentConfirmationDialog.PAYMENT_ADDRESS);
+        this.encodedEthAmount = this.view.getArguments().getString(PaymentConfirmationDialog.ETH_AMOUNT);
+        this.memo = this.view.getArguments().getString(PaymentConfirmationDialog.MEMO);
+        this.paymentType = this.view.getArguments().getInt(PaymentConfirmationDialog.PAYMENT_TYPE);
+    }
+
+    private void tryLoadUser() {
+        if (this.tokenId == null) return;
+        final Subscription sub =
+                BaseApplication
+                .get()
+                .getTokenManager()
+                .getUserManager()
+                .getUserFromAddress(this.tokenId)
+                .toSingle()
+                .doOnSuccess(user -> this.user = user)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        __ -> updateView(),
+                        this::handleUserError
+                );
+
+        this.subscriptions.add(sub);
+    }
+
+    private void handleUserError(final Throwable throwable) {
+        if (this.view == null) return;
+        Toast.makeText(
+                this.view.getContext(),
+                this.view.getString(R.string.invalid_payment),
+                Toast.LENGTH_SHORT
+        ).show();
+    }
+
+    private void updateView() {
+        setTitle();
+        setUserInfo();
+        setPaymentAddress();
+        setMemo();
+        setLocalCurrency();
+    }
+
+    private void setTitle() {
+        final String title = this.paymentType == PaymentType.TYPE_SEND
+                ? this.view.getString(R.string.confirmation_dialog_title_payment)
+                : this.view.getString(R.string.confirmation_dialog_title_request);
+        this.view.getBinding().title.setText(title);
+    }
+
+    private void setUserInfo() {
+        if (this.user == null) return;
+        this.view.getBinding().userInfoWrapper.setVisibility(View.VISIBLE);
+        ImageUtil.loadFromNetwork(user.getAvatar(), this.view.getBinding().avatar);
+        this.view.getBinding().displayName.setText(this.user.getDisplayName());
+        this.view.getBinding().username.setText(this.user.getUsername());
+        final String reviewCount = BaseApplication.get().getString(R.string.parentheses, this.user.getReviewCount());
+        this.view.getBinding().numberOfRatings.setText(reviewCount);
+        final double reputationScore = this.user.getReputationScore() != null
+                ? this.user.getReputationScore()
+                : 0;
+        this.view.getBinding().ratingView.setStars(reputationScore);
+    }
+
+    private void setPaymentAddress() {
+        if (this.paymentAddress == null) return;
+        this.view.getBinding().paymentAddress.setVisibility(View.VISIBLE);
+        final String paymentAddress = this.view.getString(R.string.payment_address_with_value, this.paymentAddress);
+        this.view.getBinding().paymentAddress.setText(paymentAddress);
+    }
+
+    private void setMemo() {
+        if (this.memo == null) return;
+        this.view.getBinding().memo.setVisibility(View.VISIBLE);
+        this.view.getBinding().memo.setText(this.memo);
+    }
+
+    private void setLocalCurrency() {
+        final BigInteger weiAmount = TypeConverter.StringHexToBigInteger(this.encodedEthAmount);
+        final BigDecimal ethAmount = EthUtil.weiToEth(weiAmount);
+
+        final Subscription sub =
+                BaseApplication
+                .get()
+                .getTokenManager()
+                .getBalanceManager()
+                .convertEthToLocalCurrencyString(ethAmount)
+                .subscribe((localCurrency) -> {
+                    final String usdEth = this.view.getString(R.string.eth_usd, localCurrency, getEthValue());
+                    this.view.getBinding().ethUsd.setText(usdEth);
+                });
+
+        this.subscriptions.add(sub);
+    }
+
+    private String getEthValue() {
+        final BigInteger eth = TypeConverter.StringHexToBigInteger(this.encodedEthAmount);
+        return EthUtil.weiAmountToUserVisibleString(eth);
     }
 
     @Override
     public void onViewDetached() {
+        this.subscriptions.clear();
         this.view = null;
     }
 
     @Override
     public void onDestroyed() {
+        this.subscriptions = null;
         this.view = null;
     }
 }
