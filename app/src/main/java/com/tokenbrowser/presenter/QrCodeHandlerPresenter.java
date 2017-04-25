994d2ea68cd67db6810e64d5a9351fcb91014a7f
Martin Tøften <martin@bakkenbaeck.no>
2017-04-25 09:45:03 +0200

    Add support for sharing external qr code url

--- app/src/main/java/com/tokenbrowser/presenter/QrCodeHandlerPresenter.java
+++ app/src/main/java/com/tokenbrowser/presenter/QrCodeHandlerPresenter.java
@@ -17,31 +17,160 @@
 
 package com.tokenbrowser.presenter;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.widget.Toast;
 
+import com.tokenbrowser.R;
+import com.tokenbrowser.exception.InvalidQrCodePayment;
+import com.tokenbrowser.model.local.QrCodePayment;
+import com.tokenbrowser.model.sofa.Payment;
+import com.tokenbrowser.util.PaymentType;
+import com.tokenbrowser.util.QrCode;
+import com.tokenbrowser.util.QrCodeType;
+import com.tokenbrowser.util.SoundManager;
+import com.tokenbrowser.view.BaseApplication;
+import com.tokenbrowser.view.activity.ChatActivity;
 import com.tokenbrowser.view.activity.QrCodeHandlerActivity;
+import com.tokenbrowser.view.fragment.DialogFragment.PaymentConfirmationDialog;
 
-public class QrCodeHandlerPresenter implements Presenter<QrCodeHandlerActivity> {
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+public class QrCodeHandlerPresenter implements
+        Presenter<QrCodeHandlerActivity>,
+        PaymentConfirmationDialog.OnPaymentConfirmationListener {
 
     private QrCodeHandlerActivity activity;
+    private CompositeSubscription subscriptions;
+    private boolean firstTimeAttaching = true;
 
     @Override
     public void onViewAttached(QrCodeHandlerActivity view) {
         this.activity = view;
+
+        if (this.firstTimeAttaching) {
+            this.firstTimeAttaching = false;
+            initLongLivingObjects();
+        }
+
         processIntentData();
+    }
+
+    private void initLongLivingObjects() {
+        this.subscriptions = new CompositeSubscription();
     }
 
     private void processIntentData() {
         final Uri data = this.activity.getIntent().getData();
+        handleIntentUri(data);
+    }
+
+    private void handleIntentUri(final Uri uri) {
+        final QrCode qrCode = new QrCode(uri.toString());
+        final @QrCodeType.Type int qrCodeType = qrCode.getQrCodeType();
+
+        if (qrCodeType == QrCodeType.EXTERNAL) {
+            handleExternalPayment(qrCode);
+        } else {
+            handleInvalidQrCodePayment();
+        }
+    }
+
+    private void handleExternalPayment(final QrCode qrCode) {
+        try {
+            final QrCodePayment payment = qrCode.getPayment();
+            final Subscription sub =
+                    BaseApplication
+                    .get()
+                    .getTokenManager()
+                    .getUserManager()
+                    .getUserFromPaymentAddress(payment.getAddress())
+                    .subscribe(
+                            user -> showTokenPaymentConfirmationDialog(user.getTokenId(), payment),
+                            __ -> showExternalPaymentConfirmationDialog(payment)
+                    );
+
+            this.subscriptions.add(sub);
+        } catch (InvalidQrCodePayment e) {
+            handleInvalidQrCodePayment();
+        }
+    }
+
+    private void showTokenPaymentConfirmationDialog(final String tokenId, final QrCodePayment payment) {
+        try {
+            final PaymentConfirmationDialog dialog =
+                    PaymentConfirmationDialog
+                            .newInstanceTokenPayment(
+                                    tokenId,
+                                    payment.getValue(),
+                                    payment.getMemo()
+                            );
+            dialog.show(this.activity.getSupportFragmentManager(), PaymentConfirmationDialog.TAG);
+            dialog.setOnPaymentConfirmationListener(this);
+        } catch (InvalidQrCodePayment e) {
+            handleInvalidQrCodePayment();
+        }
+    }
+
+    private void showExternalPaymentConfirmationDialog(final QrCodePayment payment) {
+        try {
+            final PaymentConfirmationDialog dialog =
+                    PaymentConfirmationDialog
+                            .newInstanceExternalPayment(
+                                    payment.getAddress(),
+                                    payment.getValue(),
+                                    payment.getMemo()
+                            );
+            dialog.show(this.activity.getSupportFragmentManager(), PaymentConfirmationDialog.TAG);
+            dialog.setOnPaymentConfirmationListener(this);
+        } catch (InvalidQrCodePayment e) {
+            handleInvalidQrCodePayment();
+        }
     }
 
     @Override
+    public void onPaymentRejected() {
+        this.activity.finish();
+    }
+
+    @Override
+    public void onTokenPaymentApproved(final String tokenId, final Payment payment) {
+        try {
+            goToChatActivityWithPayment(tokenId, payment);
+        } catch (InvalidQrCodePayment e) {
+            handleInvalidQrCodePayment();
+        }
+    }
+
+    private void goToChatActivityWithPayment(final String tokenId, final Payment payment) throws InvalidQrCodePayment {
+        final Intent intent = new Intent(activity, ChatActivity.class)
+                .putExtra(ChatActivity.EXTRA__REMOTE_USER_ADDRESS, tokenId)
+                .putExtra(ChatActivity.EXTRA__PAYMENT_ACTION, PaymentType.TYPE_SEND)
+                .putExtra(ChatActivity.EXTRA__ETH_AMOUNT, payment.getValue())
+                .putExtra(ChatActivity.EXTRA__PLAY_SCAN_SOUNDS, true);
+
+        this.activity.startActivity(intent);
+        this.activity.finish();
+    }
+
+    private void handleInvalidQrCodePayment() {
+        SoundManager.getInstance().playSound(SoundManager.SCAN_ERROR);
+        Toast.makeText(this.activity, this.activity.getString(R.string.invalid_payment), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onExternalPaymentApproved(final Payment payment) {}
+
+    @Override
     public void onViewDetached() {
+        this.subscriptions.clear();
         this.activity = null;
     }
 
     @Override
     public void onDestroyed() {
+        this.subscriptions = null;
         this.activity = null;
     }
 }
