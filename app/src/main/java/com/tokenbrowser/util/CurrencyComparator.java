eb317a37bcf8cdc98a5f0c622311dfdf957d3be3
Martin Tøften <martin@bakkenbaeck.no>
2017-05-18 13:13:10 +0200

    Add most common languages to the top

--- app/src/main/java/com/tokenbrowser/util/CurrencyComparator.java
+++ app/src/main/java/com/tokenbrowser/util/CurrencyComparator.java
@@ -2,11 +2,21 @@ package com.tokenbrowser.util;
 
 import com.tokenbrowser.model.network.Currency;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class CurrencyComparator implements Comparator<Currency> {
+
+    private static final List<String> popularCurrencies = Arrays.asList("USD", "EUR", "CNY", "GBP", "CAD");
+
     @Override
     public int compare(Currency o1, Currency o2) {
+        if (popularCurrencies.contains(o1.getId())) {
+            return -1;
+        } else if (popularCurrencies.contains(o2.getId())) {
+            return 1;
+        }
         return o1.getName().compareTo(o2.getName());
     }
 }
