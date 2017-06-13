7c18c2f307aa382fa0ed7f128d9ad801165793dc
Martin Tøften <martin@bakkenbaeck.no>
2017-06-13 13:31:46 +0200

    Add users to list

--- app/src/main/java/com/tokenbrowser/presenter/GroupParticipantsPresenter.java
+++ app/src/main/java/com/tokenbrowser/presenter/GroupParticipantsPresenter.java
@@ -17,24 +17,120 @@
 
 package com.tokenbrowser.presenter;
 
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.jakewharton.rxbinding.widget.RxTextView;
+import com.tokenbrowser.R;
+import com.tokenbrowser.util.LogUtil;
+import com.tokenbrowser.view.BaseApplication;
 import com.tokenbrowser.view.activity.GroupParticipantsActivity;
+import com.tokenbrowser.view.adapter.GroupParticipantAdapter;
+import com.tokenbrowser.view.custom.HorizontalLineDivider;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.subscriptions.CompositeSubscription;
 
 public class GroupParticipantsPresenter implements Presenter<GroupParticipantsActivity> {
 
     private GroupParticipantsActivity activity;
+    private CompositeSubscription subscriptions;
+
+    private boolean firstTimeAttaching = true;
 
     @Override
     public void onViewAttached(GroupParticipantsActivity view) {
         this.activity = view;
+
+        if (this.firstTimeAttaching) {
+            this.firstTimeAttaching = false;
+            initLongLivingObjects();
+        }
+        initShortLivingObjects();
+    }
+
+    private void initLongLivingObjects() {
+        this.subscriptions = new CompositeSubscription();
+    }
+
+    private void initShortLivingObjects() {
+        initRecyclerView();
+        initSearch();
+    }
+
+    private void initRecyclerView() {
+        final RecyclerView recyclerView = this.activity.getBinding().searchResults;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.activity));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        final GroupParticipantAdapter adapter = new GroupParticipantAdapter();
+        recyclerView.setAdapter(adapter);
+
+        final int dividerLeftPadding = this.activity.getResources().getDimensionPixelSize(R.dimen.avatar_size_small)
+                + this.activity.getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin);
+        final HorizontalLineDivider lineDivider =
+                new HorizontalLineDivider(ContextCompat.getColor(this.activity, R.color.divider))
+                        .setLeftPadding(dividerLeftPadding);
+        recyclerView.addItemDecoration(lineDivider);
+    }
+
+    private void initSearch() {
+        final Subscription sub =
+                RxTextView
+                .textChangeEvents(this.activity.getBinding().userInput)
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .map(event -> event.text().toString())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        this::submitQuery,
+                        this::handleSearchError
+                );
+
+        this.subscriptions.add(sub);
+    }
+
+    private void submitQuery(final String query) {
+        if (query.length() < 3) {
+            this.getParticipantsAdapter().clear();
+            return;
+        }
+
+        final Subscription sub =
+                BaseApplication
+                .get()
+                .getTokenManager()
+                .getUserManager()
+                .searchOnlineUsers(query)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        users -> getParticipantsAdapter().setUsers(users),
+                        this::handleSearchError
+                );
+
+        this.subscriptions.add(sub);
+    }
+
+    private void handleSearchError(final Throwable throwable) {
+        LogUtil.exception(getClass(), "Error while searching for user", throwable);
+    }
+
+    private GroupParticipantAdapter getParticipantsAdapter() {
+        return (GroupParticipantAdapter) this.activity.getBinding().searchResults.getAdapter();
     }
 
     @Override
     public void onViewDetached() {
+        this.subscriptions.clear();
         this.activity = null;
     }
 
     @Override
     public void onDestroyed() {
+        this.subscriptions = null;
         this.activity = null;
     }
 }
