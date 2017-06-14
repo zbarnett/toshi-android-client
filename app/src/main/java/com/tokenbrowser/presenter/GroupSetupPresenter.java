84d3bb948fd7106693fdc71cbd2bae7eabb7fe2f
Martin Tøften <martin@bakkenbaeck.no>
2017-06-14 11:52:46 +0200

    Make it possible to choose group image

--- app/src/main/java/com/tokenbrowser/presenter/GroupSetupPresenter.java
+++ app/src/main/java/com/tokenbrowser/presenter/GroupSetupPresenter.java
@@ -1,18 +1,34 @@
 package com.tokenbrowser.presenter;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.tokenbrowser.BuildConfig;
 import com.tokenbrowser.R;
+import com.tokenbrowser.model.local.ActivityResultHolder;
+import com.tokenbrowser.model.local.PermissionResultHolder;
 import com.tokenbrowser.model.local.User;
+import com.tokenbrowser.util.FileUtil;
+import com.tokenbrowser.util.ImageUtil;
 import com.tokenbrowser.util.LogUtil;
+import com.tokenbrowser.util.PermissionUtil;
 import com.tokenbrowser.view.BaseApplication;
 import com.tokenbrowser.view.activity.GroupSetupActivity;
 import com.tokenbrowser.view.adapter.GroupParticipantAdapter;
 import com.tokenbrowser.view.custom.HorizontalLineDivider;
+import com.tokenbrowser.view.fragment.DialogFragment.ChooserDialog;
 
+import java.io.File;
 import java.util.List;
 
 import rx.Observable;
@@ -22,9 +38,17 @@ import rx.subscriptions.CompositeSubscription;
 
 public class GroupSetupPresenter implements Presenter<GroupSetupActivity> {
 
+    private static final int PICK_IMAGE = 1;
+    private static final int CAPTURE_IMAGE = 2;
+    private static final String INTENT_TYPE = "image/*";
+    private static final String CAPTURED_IMAGE_PATH = "capturedImagePath";
+
     private GroupSetupActivity activity;
     private CompositeSubscription subscriptions;
+
     private boolean firstTimeAttaching = true;
+    private String capturedImagePath;
+    private ChooserDialog chooserDialog;
 
     @Override
     public void onViewAttached(GroupSetupActivity view) {
@@ -45,11 +69,13 @@ public class GroupSetupPresenter implements Presenter<GroupSetupActivity> {
         initClickListeners();
         initRecyclerView();
         initNumberOfParticipantsView();
+        initAvatarPlaceholder();
         fetchUsers();
     }
 
     private void initClickListeners() {
         this.activity.getBinding().closeButton.setOnClickListener(__ -> this.activity.finish());
+        this.activity.getBinding().avatar.setOnClickListener(__ -> handleAvatarClicked());
     }
 
     private void initRecyclerView() {
@@ -70,6 +96,13 @@ public class GroupSetupPresenter implements Presenter<GroupSetupActivity> {
     private void initNumberOfParticipantsView() {
         final String participants = this.activity.getString(R.string.number_of_participants, String.format("%d", getParticipantList().size()));
         this.activity.getBinding().numberOfParticipants.setText(participants);
+    }
+
+    private void initAvatarPlaceholder() {
+        ImageUtil.renderResourceIntoTarget(
+                R.drawable.ic_camera_with_background,
+                this.activity.getBinding().avatar
+        );
     }
 
     private void fetchUsers() {
@@ -104,6 +137,119 @@ public class GroupSetupPresenter implements Presenter<GroupSetupActivity> {
 
     private void HandleError(final Throwable throwable) {
         LogUtil.exception(getClass(), "Error during fetching group participants", throwable);
+    }
+
+    private void handleAvatarClicked() {
+        this.chooserDialog = ChooserDialog.newInstance();
+        this.chooserDialog.setOnChooserClickListener(new ChooserDialog.OnChooserClickListener() {
+            @Override
+            public void captureImageClicked() {
+                checkCameraPermission();
+            }
+
+            @Override
+            public void importImageFromGalleryClicked() {
+                checkExternalStoragePermission();
+            }
+        });
+        this.chooserDialog.show(this.activity.getSupportFragmentManager(), ChooserDialog.TAG);
+    }
+
+    private void checkExternalStoragePermission() {
+        final boolean hasPermission = PermissionUtil.hasPermission(this.activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (hasPermission) {
+            startGalleryActivity();
+        } else {
+            PermissionUtil.requestPermission(
+                    this.activity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    PermissionUtil.READ_EXTERNAL_STORAGE_PERMISSION
+            );
+        }
+    }
+
+    private void checkCameraPermission() {
+        final boolean hasPermission = PermissionUtil.hasPermission(this.activity, Manifest.permission.CAMERA);
+        if (hasPermission) {
+            startCameraActivity();
+        } else {
+            PermissionUtil.requestPermission(
+                    this.activity,
+                    Manifest.permission.CAMERA,
+                    PermissionUtil.CAMERA_PERMISSION
+            );
+        }
+    }
+
+    private void startCameraActivity() {
+        final Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(this.activity.getPackageManager()) != null) {
+            final File photoFile = new FileUtil().createImageFileWithRandomName();
+            this.capturedImagePath = photoFile.getAbsolutePath();
+            final Uri photoURI = FileProvider.getUriForFile(
+                    BaseApplication.get(),
+                    BuildConfig.APPLICATION_ID + FileUtil.FILE_PROVIDER_NAME,
+                    photoFile);
+            PermissionUtil.grantUriPermission(this.activity, cameraIntent, photoURI);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            this.activity.startActivityForResult(cameraIntent, CAPTURE_IMAGE);
+        }
+    }
+
+    private void startGalleryActivity() {
+        final Intent pickPictureIntent = new Intent()
+                .setType(INTENT_TYPE)
+                .setAction(Intent.ACTION_GET_CONTENT);
+
+        if (pickPictureIntent.resolveActivity(this.activity.getPackageManager()) != null) {
+            final Intent chooser = Intent.createChooser(
+                    pickPictureIntent,
+                    BaseApplication.get().getString(R.string.select_picture));
+            this.activity.startActivityForResult(chooser, PICK_IMAGE);
+        }
+    }
+
+    public boolean handleActivityResult(final ActivityResultHolder resultHolder) {
+        if (resultHolder.getResultCode() != Activity.RESULT_OK
+                || this.activity == null) {
+            return false;
+        }
+
+        if (resultHolder.getRequestCode() == PICK_IMAGE) {
+            final Uri imageUri = resultHolder.getIntent().getData();
+            ImageUtil.renderFileIntoTarget(imageUri, this.activity.getBinding().avatar);
+        } else if (resultHolder.getRequestCode() == CAPTURE_IMAGE) {
+            final File file = new File(this.capturedImagePath);
+            final Uri imageUri = Uri.fromFile(file);
+            ImageUtil.renderFileIntoTarget(imageUri, this.activity.getBinding().avatar);
+        }
+
+        return true;
+    }
+
+    public boolean handlePermissionResult(final PermissionResultHolder permissionResultHolder) {
+        if (permissionResultHolder == null || this.activity == null) return false;
+        final int[] grantResults = permissionResultHolder.getGrantResults();
+        if (grantResults.length == 0) return true;
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) return true;
+
+        if (permissionResultHolder.getRequestCode() == PermissionUtil.CAMERA_PERMISSION) {
+            startCameraActivity();
+            return true;
+        } else if (permissionResultHolder.getRequestCode() == PermissionUtil.READ_EXTERNAL_STORAGE_PERMISSION) {
+            startGalleryActivity();
+            return true;
+        }
+
+        return false;
+    }
+
+    public void onSaveInstanceState(final Bundle outState) {
+        outState.putString(CAPTURED_IMAGE_PATH, this.capturedImagePath);
+    }
+
+    public void onRestoreInstanceState(final Bundle inState) {
+        this.capturedImagePath = inState.getString(CAPTURED_IMAGE_PATH);
     }
 
     @Override
--- app/src/main/java/com/tokenbrowser/view/activity/GroupSetupActivity.java
+++ app/src/main/java/com/tokenbrowser/view/activity/GroupSetupActivity.java
@@ -1,11 +1,14 @@
 package com.tokenbrowser.view.activity;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 
 import com.tokenbrowser.R;
 import com.tokenbrowser.databinding.ActivityGroupSetupBinding;
+import com.tokenbrowser.model.local.ActivityResultHolder;
+import com.tokenbrowser.model.local.PermissionResultHolder;
 import com.tokenbrowser.presenter.GroupSetupPresenter;
 import com.tokenbrowser.presenter.LoaderIds;
 import com.tokenbrowser.presenter.factory.GroupSetupPresenterFactory;
@@ -16,6 +19,9 @@ public class GroupSetupActivity extends BasePresenterActivity<GroupSetupPresente
     public static final String PARTICIPANTS = "PARTICIPANTS";
 
     private ActivityGroupSetupBinding binding;
+    private PermissionResultHolder permissionResultHolder;
+    private ActivityResultHolder resultHolder;
+    private GroupSetupPresenter presenter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,7 +44,64 @@ public class GroupSetupActivity extends BasePresenterActivity<GroupSetupPresente
     }
 
     @Override
-    protected void onPresenterPrepared(@NonNull GroupSetupPresenter presenter) {}
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        this.resultHolder = new ActivityResultHolder(requestCode, resultCode, data);
+        tryProcessResultHolder();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(final int requestCode,
+                                           @NonNull final String permissions[],
+                                           @NonNull final int[] grantResults) {
+        this.permissionResultHolder = new PermissionResultHolder(requestCode, permissions, grantResults);
+        tryProcessPermissionResultHolder();
+    }
+
+    private void tryProcessPermissionResultHolder() {
+        if (this.presenter == null || this.permissionResultHolder == null) return;
+
+        if (this.presenter.handlePermissionResult(this.permissionResultHolder)) {
+            this.permissionResultHolder = null;
+        }
+    }
+
+    private void tryProcessResultHolder() {
+        if (this.presenter == null || this.resultHolder == null) return;
+
+        if (this.presenter.handleActivityResult(this.resultHolder)) {
+            this.resultHolder = null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        tryProcessResultHolder();
+        tryProcessPermissionResultHolder();
+    }
+
+    @Override
+    public void onSaveInstanceState(final Bundle outState) {
+        this.presenter.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(final Bundle inState) {
+        super.onSaveInstanceState(inState);
+        this.presenter.onRestoreInstanceState(inState);
+    }
+
+    @Override
+    protected void onPresenterDestroyed() {
+        super.onPresenterDestroyed();
+        this.presenter = null;
+    }
+
+    @Override
+    protected void onPresenterPrepared(@NonNull GroupSetupPresenter presenter) {
+        this.presenter = presenter;
+    }
 
     @Override
     protected int loaderId() {
