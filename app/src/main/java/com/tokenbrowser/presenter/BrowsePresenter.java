1d0f4953ab9486e4c4fe5ce93f57d39977bf3f8b
Martin Tøften <martin@bakkenbaeck.no>
2017-06-23 14:17:25 +0200

    Add users/apps to list

--- app/src/main/java/com/tokenbrowser/presenter/BrowsePresenter.java
+++ app/src/main/java/com/tokenbrowser/presenter/BrowsePresenter.java
@@ -17,10 +17,28 @@
 
 package com.tokenbrowser.presenter;
 
-import com.tokenbrowser.R;
-import com.tokenbrowser.util.BrowseType;
-import com.tokenbrowser.view.activity.BrowseActivity;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.tokenbrowser.R;
+import com.tokenbrowser.manager.UserManager;
+import com.tokenbrowser.model.local.TokenEntity;
+import com.tokenbrowser.model.local.User;
+import com.tokenbrowser.model.network.App;
+import com.tokenbrowser.util.BrowseType;
+import com.tokenbrowser.util.LogUtil;
+import com.tokenbrowser.view.BaseApplication;
+import com.tokenbrowser.view.activity.BrowseActivity;
+import com.tokenbrowser.view.activity.ViewUserActivity;
+import com.tokenbrowser.view.adapter.BrowseAdapter;
+import com.tokenbrowser.view.custom.HorizontalLineDivider;
+
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
 import static com.tokenbrowser.util.BrowseType.VIEW_TYPE_LATEST_APPS;
@@ -52,6 +70,8 @@ public class BrowsePresenter implements Presenter<BrowseActivity> {
     private void initShortLivingObjects() {
         initToolbar();
         initClickListeners();
+        initRecyclerView();
+        fetchData();
     }
 
     private void initToolbar() {
@@ -80,9 +100,119 @@ public class BrowsePresenter implements Presenter<BrowseActivity> {
         this.activity.getBinding().closeButton.setOnClickListener(__ -> this.activity.finish());
     }
 
+    private void initRecyclerView() {
+        final RecyclerView recyclerView = this.activity.getBinding().browseList;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.activity));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        final int dividerLeftPadding = this.activity.getResources().getDimensionPixelSize(R.dimen.avatar_size_small)
+                + this.activity.getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin)
+                + this.activity.getResources().getDimensionPixelSize(R.dimen.list_item_avatar_margin);
+        final HorizontalLineDivider lineDivider =
+                new HorizontalLineDivider(ContextCompat.getColor(this.activity, R.color.divider))
+                        .setLeftPadding(dividerLeftPadding);
+        recyclerView.addItemDecoration(lineDivider);
+
+        final boolean isPublicUserViewType = getViewType() == VIEW_TYPE_LATEST_PUBLIC_USERS
+                || getViewType() == VIEW_TYPE_TOP_RATED_PUBLIC_USERS;
+
+        final BrowseAdapter adapter = isPublicUserViewType
+                ? new BrowseAdapter<User>()
+                : new BrowseAdapter<App>();
+        adapter.setOnItemClickListener(this::handleItemClicked);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void handleItemClicked(final Object elem) {
+        final Intent intent = new Intent(this.activity, ViewUserActivity.class)
+                .putExtra(ViewUserActivity.EXTRA__USER_ADDRESS, ((TokenEntity) elem).getTokenId());
+        this.activity.startActivity(intent);
+    }
+
+    private void fetchData() {
+        if (getViewType() == VIEW_TYPE_TOP_RATED_APPS) {
+            fetchTopRatedApps();
+        } else if (getViewType() == VIEW_TYPE_LATEST_APPS) {
+            fetchLatestApps();
+        } else if (getViewType() == VIEW_TYPE_TOP_RATED_PUBLIC_USERS) {
+            fetchTopRatedPublicUsers();
+        } else {
+            fetchLatestPublicUsers();
+        }
+    }
+
     @SuppressWarnings("WrongConstant")
     private @BrowseType.Type int getViewType() {
         return this.activity.getIntent().getIntExtra(BrowseActivity.VIEW_TYPE, VIEW_TYPE_LATEST_PUBLIC_USERS);
+    }
+
+    private void fetchTopRatedApps() {
+        final Subscription sub =
+                getUserManager()
+                .getTopRatedApps(100)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        apps -> getAdapter().setItems(apps),
+                        this::handleError
+                );
+
+        this.subscriptions.add(sub);
+    }
+
+    private void fetchLatestApps() {
+        final Subscription sub =
+                getUserManager()
+                .getLatestApps(100)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        apps -> getAdapter().setItems(apps),
+                        this::handleError
+                );
+
+        this.subscriptions.add(sub);
+    }
+
+    private void fetchTopRatedPublicUsers() {
+        final Subscription sub =
+                getUserManager()
+                .getTopRatedPublicUsers(100)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        users -> getAdapter().setItems(users),
+                        this::handleError
+                );
+
+        this.subscriptions.add(sub);
+    }
+
+    private void fetchLatestPublicUsers() {
+        final Subscription sub =
+                getUserManager()
+                .getLatestPublicUsers(100)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        users -> getAdapter().setItems(users),
+                        this::handleError
+                );
+
+        this.subscriptions.add(sub);
+    }
+
+    private UserManager getUserManager() {
+        return BaseApplication
+                .get()
+                .getTokenManager()
+                .getUserManager();
+    }
+
+    private BrowseAdapter getAdapter() {
+        return (BrowseAdapter) this.activity.getBinding().browseList.getAdapter();
+    }
+
+    private void handleError(final Throwable throwable) {
+        LogUtil.exception(getClass(), "Error fetching app/public users", throwable);
     }
 
     @Override
