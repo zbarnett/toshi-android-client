85fa845b6d33c38f02b1f3401ca65b9f779b5ac4
csdodd <colin@bakkenbaeck.no>
2017-08-09 11:54:48 +0200

    Update license.

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Making a new build for Play store (Dev only)
 
 
 ```
-	Copyright (c) 2017 Toshi Browser, Inc
+	Copyright (c) 2017 Toshi Inc
 
 	This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- app/src/main/java/com/toshi/crypto/signal/util/PreKeyUtil.java
+++ app/src/main/java/com/toshi/crypto/signal/util/PreKeyUtil.java
@@ -1,5 +1,5 @@
 /*
- * 	Copyright (c) 2017. Toshi Browser, Inc
+ * 	Copyright (c) 2017. Toshi Inc
  *
  * 	This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
  */
 
 /*
- * 	Copyright (c) 2017. Toshi Browser, Inc
+ * 	Copyright (c) 2017. Toshi Inc
  *
  * 	This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
