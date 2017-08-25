ce34ad782cd2b51eb7ba1dbc66732d6cfdc51143
Martin Tøften <martin@bakkenbaeck.no>
2017-08-25 10:32:16 +0200

    Remove current solution

--- app/src/main/java/com/toshi/presenter/SignInPresenter.java
+++ app/src/main/java/com/toshi/presenter/SignInPresenter.java
@@ -18,45 +18,28 @@
 package com.toshi.presenter;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.ColorRes;
 import android.support.annotation.StringRes;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v4.util.Pair;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import com.beloo.widget.chipslayoutmanager.ChipsLayoutManager;
 import com.google.common.base.Joiner;
-import com.jakewharton.rxbinding.widget.RxTextView;
 import com.toshi.R;
 import com.toshi.crypto.HDWallet;
 import com.toshi.manager.ToshiManager;
-import com.toshi.util.SearchUtil;
 import com.toshi.util.LogUtil;
 import com.toshi.util.SharedPrefsUtil;
 import com.toshi.view.BaseApplication;
 import com.toshi.view.activity.MainActivity;
 import com.toshi.view.activity.SignInActivity;
 import com.toshi.view.activity.SignInInfoActivity;
-import com.toshi.view.adapter.SignInPassphraseAdapter;
-import com.toshi.view.custom.SpaceDecoration;
 
 import org.bitcoinj.crypto.MnemonicCode;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import rx.Observable;
 import rx.Single;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -72,9 +55,6 @@ public class SignInPresenter implements Presenter<SignInActivity> {
     private CompositeSubscription subscriptions;
 
     private boolean onGoingTask = false;
-    private List<String> wordList;
-    private List<String> approvedWords;
-    private boolean isHidden;
 
     @Override
     public void onViewAttached(SignInActivity view) {
@@ -89,195 +69,34 @@ public class SignInPresenter implements Presenter<SignInActivity> {
 
     private void initLongLivingObjects() {
         this.subscriptions = new CompositeSubscription();
-        initLists();
         initWordList();
     }
 
     private void initShortLivingObjects() {
-        initPassphraseList();
-        initPassphraseView();
         initClickListeners();
-        updateSignInView();
-    }
-
-    private void initLists() {
-        this.wordList = new ArrayList<>();
-        this.approvedWords = new ArrayList<>();
+        disableSignIn(this.activity.getString(R.string.sign_in));
     }
 
     private void initWordList() {
         try {
-            this.wordList = new MnemonicCode().getWordList();
+            final List<String> wordList = new MnemonicCode().getWordList();
         } catch (IOException e) {
             LogUtil.e(getClass(), e.toString());
         }
     }
 
-    private void initPassphraseList() {
-        final RecyclerView passphraseList = this.activity.getBinding().passphraseList;
-        if (passphraseList.getAdapter() != null) return;
-
-        final ChipsLayoutManager chipsLayoutManager = ChipsLayoutManager.newBuilder(this.activity)
-                .setRowStrategy(ChipsLayoutManager.STRATEGY_DEFAULT)
-                .setMaxViewsInRow(4)
-                .build();
-        final int itemSpacing = this.activity.getResources().getDimensionPixelSize(R.dimen.passphrase_sign_in_spacing);
-        passphraseList.addItemDecoration(new SpaceDecoration(itemSpacing));
-        passphraseList.setLayoutManager(chipsLayoutManager);
-        final SignInPassphraseAdapter adapter = new SignInPassphraseAdapter(this.approvedWords)
-                .setOnItemClickListener(this::handleWordClicked);
-        passphraseList.setAdapter(adapter);
-
-        if (this.isHidden) {
-            adapter.hideWords();
-        } else {
-            adapter.showWords();
+    private void updateSignInButton(final int approvedWords) {
+        final int wordsLeft = PASSPHRASE_LENGTH - approvedWords;
+        if (wordsLeft > 0) {
+            final String wordsLeftString = this.activity.getResources().getQuantityString(R.plurals.words, wordsLeft, wordsLeft);
+            disableSignIn(wordsLeftString);
         }
-    }
-
-    private void handleWordClicked(final int position) {
-        this.approvedWords.remove(position);
-        final SignInPassphraseAdapter adapter = (SignInPassphraseAdapter) this.activity.getBinding().passphraseList.getAdapter();
-        adapter.setPassphrase(this.approvedWords);
-        updateSignInView();
-    }
-
-    private void initPassphraseView() {
-        this.activity.getBinding().suggestionView.setOnPasteListener(this::handlePastedString);
-
-        final Subscription sub =
-                RxTextView
-                .textChanges(this.activity.getBinding().suggestionView.getWordView())
-                .skip(1)
-                .map(CharSequence::toString)
-                .doOnNext(this::clearSuggestion)
-                .filter(input -> input.length() > 0)
-                .doOnNext(__ -> hideErrorMessage())
-                .observeOn(Schedulers.io())
-                .flatMap(this::getWordSuggestion)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        this::handleWordSuggestion,
-                        throwable -> LogUtil.e(getClass(), throwable.toString())
-                );
-
-        this.subscriptions.add(sub);
-    }
-
-    private void handlePastedString(final String pastedString) {
-        final List<String> pastedPassphrase = Arrays.asList(pastedString.split(" "));
-
-        if (pastedPassphrase.size() != 12) {
-            showErrorMessage(this.activity.getString(R.string.passphrase_must_be_12_words));
-            return;
-        }
-
-        final Subscription sub =
-                Single.fromCallable(() -> sortPassphrase(pastedPassphrase))
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        listPair -> handlePastedListResult(listPair.first, listPair.second),
-                        throwable -> LogUtil.e(getClass(), throwable.toString())
-                );
-
-        this.subscriptions.add(sub);
-    }
-
-    private Pair<List<String>, List<String>> sortPassphrase(final List<String> pastedPassphrase) {
-        final List<String> approvedWords = new ArrayList<>();
-        final List<String> notApprovedWords = new ArrayList<>();
-        for (final String word : pastedPassphrase) {
-            final String result = new SearchUtil<String>().findMatch(this.wordList, word);
-            if (result != null) {
-                approvedWords.add(word);
-            } else {
-                notApprovedWords.add(word);
-            }
-        }
-        return new Pair<>(approvedWords, notApprovedWords);
-    }
-
-    private void handlePastedListResult(final List<String> approvedWords,
-                                        final List<String> notApprovedWords) {
-        this.approvedWords.clear();
-        this.approvedWords.addAll(approvedWords);
-        addWordsToAdapter(this.approvedWords);
-        updateSignInView();
-
-        if (approvedWords.size() != 12) {
-            final String s = Joiner.on(", ").join(notApprovedWords);
-            final String errorMessage = this.activity.getString(R.string.pasted_passphrase_error_message, s);
-            showErrorMessage(errorMessage);
-        }
-    }
-
-    private void clearSuggestion(final String string) {
-        if (string.length() > 0) return;
-        this.activity.getBinding().suggestionView.clearSuggestion();
-    }
-
-    private Observable<String> getWordSuggestion(final String startOfWord) {
-        return Observable.fromCallable(() -> {
-            if (startOfWord.length() == 0) return null;
-            final String suggestion = new SearchUtil<String>().findSuggestion(this.wordList, startOfWord);
-            if (suggestion == null || !suggestion.startsWith(startOfWord)) return null;
-            return suggestion;
-        });
-    }
-
-    private void handleWordSuggestion(final String suggestion) {
-        final int lengthOfPassphraseWord = this.activity.getBinding().suggestionView.getWordView().length();
-        //If the passphrase field is cleared, don't show a suggestion or an error
-        if (lengthOfPassphraseWord == 0) return;
-
-        if (suggestion == null) {
-            this.activity.getBinding().suggestionView.clearSuggestion();
-            final String passphraseInput = this.activity.getBinding().suggestionView.getWordView().getText().toString();
-            showErrorMessage(this.activity.getString(R.string.no_suggestion_found, passphraseInput));
-            return;
-        }
-
-        this.activity.getBinding().suggestionView.setSuggestion(suggestion);
-    }
-
-    private void showErrorMessage(final String message) {
-        this.activity.getBinding().hidePassphrase.setVisibility(View.GONE);
-        this.activity.getBinding().errorView.setVisibility(View.VISIBLE);
-        this.activity.getBinding().errorView.setText(message);
-        updateInputUnderline(R.color.error_color);
-    }
-
-    private void hideErrorMessage() {
-        this.activity.getBinding().hidePassphrase.setVisibility(View.VISIBLE);
-        this.activity.getBinding().errorView.setVisibility(View.GONE);
-        updateInputUnderline(R.color.input_underline);
-    }
-
-    private void updateInputUnderline(final @ColorRes int colorRes) {
-        final EditText passphrase = this.activity.getBinding().suggestionView.getWordView();
-        final Drawable originalDrawable = passphrase.getBackground();
-        final int color = ContextCompat.getColor(this.activity, colorRes);
-        DrawableCompat.setTintList(originalDrawable, ColorStateList.valueOf(color));
-        passphrase.setBackground(originalDrawable);
     }
 
     private void initClickListeners() {
         this.activity.getBinding().closeButton.setOnClickListener(__ -> this.activity.finish());
-        this.activity.getBinding().hidePassphrase.setOnClickListener(__ -> handleHidePassphraseClicked());
         this.activity.getBinding().infoView.setOnClickListener(__ -> handleInfoViewClicked());
-        this.activity.getBinding().suggestionView.getWordView().setOnEditorActionListener((__, actionId, ___) -> handleEnterClicked(actionId));
         this.activity.getBinding().signIn.setOnClickListener(v -> handleSignInClicked());
-    }
-
-    private void handleHidePassphraseClicked() {
-        this.isHidden = !this.isHidden;
-        final SignInPassphraseAdapter adapter = (SignInPassphraseAdapter) this.activity.getBinding().passphraseList.getAdapter();
-        if (this.isHidden) {
-            adapter.hideWords();
-        } else {
-            adapter.showWords();
-        }
     }
 
     private void handleInfoViewClicked() {
@@ -285,76 +104,29 @@ public class SignInPresenter implements Presenter<SignInActivity> {
         this.activity.startActivity(intent);
     }
 
-    private boolean handleEnterClicked(final int actionId) {
-        if (actionId == EditorInfo.IME_ACTION_DONE) {
-            final String word = this.activity.getBinding().suggestionView.getSuggestioniew().getText().toString();
-            if (!word.isEmpty()) {
-                addWordToList(word);
-            }
-            return true;
-        }
-        return false;
-    }
-
-    private void addWordToList(final String word) {
-        this.activity.getBinding().suggestionView.clear();
-        addWord(word);
-        updateSignInView();
-    }
-
-    private void addWord(final String word) {
-        this.approvedWords.add(word);
-        addWordsToAdapter(this.approvedWords);
-    }
-
-    private void addWordsToAdapter(final List<String> wordList) {
-        final SignInPassphraseAdapter adapter = (SignInPassphraseAdapter) this.activity.getBinding().passphraseList.getAdapter();
-        adapter.setPassphrase(wordList);
-    }
-
-    private void updateSignInView() {
-        if (this.approvedWords.size() == 0) {
-            disableSignIn(this.activity.getString(R.string.sign_in));
-            return;
-        }
-
-        final int wordsLeft = PASSPHRASE_LENGTH - this.approvedWords.size();
-        if (wordsLeft > 0) {
-            final String wordsLeftString = this.activity.getResources().getQuantityString(R.plurals.words, wordsLeft, wordsLeft);
-            disableSignIn(wordsLeftString);
-        } else if (wordsLeft == 0) {
-            enableSignIn();
-        }
-    }
-
     private void disableSignIn(final String string) {
         final Button signIn = this.activity.getBinding().signIn;
         signIn.setText(string);
         signIn.setBackgroundResource(R.drawable.background_with_radius_disabled);
         signIn.setEnabled(false);
-
-        this.activity.getBinding().messageWrapper.setVisibility(View.VISIBLE);
-        this.activity.getBinding().suggestionView.setVisibility(View.VISIBLE);
     }
 
-    private void enableSignIn() {
+    private void handlePassphraseFinished() {
+        if (this.activity == null) return;
         final Button signIn = this.activity.getBinding().signIn;
         signIn.setText(R.string.sign_in);
         signIn.setBackgroundResource(R.drawable.background_with_radius_primary_color);
         signIn.setEnabled(true);
-
-        this.activity.getBinding().messageWrapper.setVisibility(View.GONE);
-        this.activity.getBinding().suggestionView.setVisibility(View.GONE);
     }
 
-    private void handleSignInClicked() {
-        if (this.approvedWords.size() != PASSPHRASE_LENGTH) {
+    private void handleSignInClicked(final List<String> approvedWords) {
+        if (approvedWords.size() != PASSPHRASE_LENGTH) {
             showToast(R.string.sign_in_length_error_message);
             return;
         }
 
         final Joiner joiner = Joiner.on(" ");
-        final String masterSeed = joiner.join(this.approvedWords);
+        final String masterSeed = joiner.join(approvedWords);
         tryCreateWallet(masterSeed);
     }
 
