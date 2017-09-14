1860e845ea65b4ac39b20458ea2010bdf024d514
csdodd <colinsdodd@gmail.com>
2017-09-14 15:56:07 +0200

    Update google service config. Rename "beta" to "developer"

--- app/src/main/java/com/toshi/util/BuildTypes.java
+++ app/src/main/java/com/toshi/util/BuildTypes.java
@@ -2,6 +2,6 @@ package com.toshi.util;
 
 public class BuildTypes {
     public static final String RELEASE = "release";
-    public static final String BETA = "beta";
+    public static final String DEVELOPER = "developer";
     public static final String DEBUG = "debug";
 }
