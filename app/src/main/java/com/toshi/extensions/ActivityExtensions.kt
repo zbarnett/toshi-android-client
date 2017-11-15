a92ce904f1615116df5ee4be417280128afb24c8
martintoften <martin@bakkenbaeck.no>
2017-11-15 10:10:13 +0100

    Add activity and view extensions

--- app/src/main/java/com/toshi/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/toshi/extensions/ActivityExtensions.kt
@@ -1,10 +1,44 @@
+/*
+ * 	Copyright (c) 2017. Toshi Inc
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.toshi.extensions
 
 import android.content.Intent
 import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.content.res.AppCompatResources
+import android.widget.Toast
 
 fun AppCompatActivity.getColorById(@ColorRes id: Int) = ContextCompat.getColor(this, id)
 
 inline fun <reified T> AppCompatActivity.startActivity() = startActivity(Intent(this, T::class.java))
+
+fun AppCompatActivity.toast(@StringRes id: Int) = Toast.makeText(this, id, Toast.LENGTH_SHORT).show()
+
+inline fun <reified T> AppCompatActivity.startActivityAndFinish(func: Intent.() -> Intent) {
+    startActivity(Intent(this, T::class.java).func())
+    finish()
+}
+
+inline fun <reified T> AppCompatActivity.startActivityForResult(requestCode: Int, func: Intent.() -> Intent) {
+    startActivityForResult(Intent(this, T::class.java).func(), requestCode)
+}
+
+fun AppCompatActivity.getDrawableById(@DrawableRes id: Int) = AppCompatResources.getDrawable(this, id)
