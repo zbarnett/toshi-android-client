0fa8b36576c2377244deaa493adeb5d94e12c9d9
martintoften <martin@bakkenbaeck.no>
2017-11-22 12:49:43 +0100

    Add conversation request view

---
+++ app/src/main/java/com/toshi/view/custom/ConversationRequestView.kt
@@ -0,0 +1,48 @@
+/*
+ * 	Copyright (c) 2017. Toshi Inc
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.toshi.view.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import com.toshi.R
+import kotlinx.android.synthetic.main.view_conversation_request.view.*
+
+class ConversationRequestView : LinearLayout {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
+        init()
+    }
+
+    var onAcceptClickListener: (() -> Unit)? = null
+    var onDeclineClickListener: (() -> Unit)? = null
+
+    private fun init() {
+        val view = inflate(context, R.layout.view_conversation_request, this)
+        view.accept.setOnClickListener { onAcceptClickListener?.invoke() }
+        view.decline.setOnClickListener { onDeclineClickListener?.invoke() }
+    }
+}
