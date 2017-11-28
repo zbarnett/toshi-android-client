d5147ab68aaf60f35af663fe9df3fed4bcf39823
martintoften <martin@bakkenbaeck.no>
2017-11-28 16:40:41 +0100

    Rewrite ViewProfile*

---
+++ app/src/main/java/com/toshi/view/activity/ViewProfileActivity.kt
@@ -0,0 +1,101 @@
+/*
+ * 	Copyright (c) 2017. Toshi Inc
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.toshi.view.activity
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.toshi.R
+import com.toshi.extensions.startActivity
+import com.toshi.model.local.User
+import com.toshi.model.network.ReputationScore
+import com.toshi.util.ImageUtil
+import com.toshi.viewModel.ViewProfileViewModel
+import kotlinx.android.synthetic.main.activity_view_profile.*
+
+class ViewProfileActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ViewProfileViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile)
+        init()
+    }
+
+    private fun init() {
+        initViewModel()
+        initClickListeners()
+        initObservers()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(ViewProfileViewModel::class.java)
+    }
+
+    private fun initClickListeners() {
+        closeButton.setOnClickListener { finish() }
+        editProfileButton.setOnClickListener { startActivity<EditProfileActivity>() }
+        avatar.setOnClickListener { startFullscreenImageActivity() }
+    }
+
+    private fun startFullscreenImageActivity() {
+        val avatar = viewModel.user.value?.avatar
+        avatar?.let {
+            startActivity<FullscreenImageActivity> { putExtra(FullscreenImageActivity.IMAGE_URL, it) }
+        }
+    }
+
+    private fun initObservers() {
+        viewModel.user.observe(this, Observer {
+            user -> user?.let { updateView(it) } ?: handleNoUser()
+        })
+        viewModel.reputation.observe(this, Observer {
+            reputation -> reputation?.let { handleReputation(it) }
+        })
+        viewModel.isConnected.observe(this, Observer {
+            isConnected -> isConnected?.let { editProfileButton.isEnabled = isConnected }
+        })
+    }
+
+    private fun updateView(localUser: User) {
+        name.text = localUser.displayName
+        username.text = localUser.username
+        about.text = localUser.about
+        location.text = localUser.location
+        ImageUtil.load(localUser.avatar, avatar)
+    }
+
+    private fun handleNoUser() {
+        name.text = getString(R.string.profile__unknown_name)
+        username.text = ""
+        about.text = ""
+        location.text = ""
+        ratingView.setStars(0.0)
+    }
+
+    private fun handleReputation(reputation: ReputationScore) {
+        val revCount = reputation.reviewCount
+        val ratingText = resources.getQuantityString(R.plurals.ratings, revCount, revCount)
+        reviewCount.text = ratingText
+        ratingView.setStars(reputation.averageRating)
+        reputationScore.text = reputation.averageRating.toString()
+        ratingInfo.setRatingInfo(reputation)
+    }
+}
---
+++ app/src/main/java/com/toshi/viewModel/ViewProfileViewModel.kt
@@ -0,0 +1,84 @@
+/*
+ * 	Copyright (c) 2017. Toshi Inc
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.toshi.viewModel
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.toshi.model.local.User
+import com.toshi.model.network.ReputationScore
+import com.toshi.view.BaseApplication
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import rx.subscriptions.CompositeSubscription
+
+class ViewProfileViewModel : ViewModel() {
+
+    private val subscriptions by lazy { CompositeSubscription() }
+    private val reputationManager by lazy { BaseApplication.get().reputationManager }
+    private val userManager by lazy { BaseApplication.get().userManager }
+
+    val user by lazy { MutableLiveData<User>() }
+    val isConnected by lazy { MutableLiveData<Boolean>() }
+    val reputation by lazy { MutableLiveData<ReputationScore>() }
+
+    init {
+        fetchUser()
+        attachConnectionListeners()
+    }
+
+    private fun fetchUser() {
+        val sub = userManager
+                .userObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { user.value = it; fetchUserReputation(it.toshiId) },
+                        { user.value = null }
+                )
+
+        this.subscriptions.add(sub)
+    }
+
+    private fun attachConnectionListeners() {
+        val sub = BaseApplication.get()
+                .isConnectedSubject
+                .subscribe(
+                        { isConnected.value = it },
+                        { isConnected.value = false }
+                )
+
+        this.subscriptions.add(sub)
+    }
+
+    private fun fetchUserReputation(userAddress: String) {
+        val reputationSub = reputationManager
+                .getReputationScore(userAddress)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { reputation.value = it },
+                        { reputation.value = null }
+                )
+
+        this.subscriptions.add(reputationSub)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        subscriptions.clear()
+    }
+}
