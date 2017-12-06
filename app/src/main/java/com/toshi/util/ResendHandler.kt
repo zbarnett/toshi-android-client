2cb1b0bdf531ea9847f8dc61c2358bfd9342628b
martintoften <martin@bakkenbaeck.no>
2017-12-06 15:01:48 +0100

    Rewrite ResendHandler from Java to Kotlin
    No functional changes to the code, aside from tweaking the callbacks to be more in the style of Kotlin

---
+++ app/src/main/java/com/toshi/util/ResendHandler.kt
@@ -0,0 +1,69 @@
+package com.toshi.util
+
+import android.support.design.widget.BottomSheetDialog
+import android.support.v7.app.AppCompatActivity
+import android.widget.LinearLayout
+import com.toshi.R
+import com.toshi.model.local.User
+import com.toshi.model.network.UnsignedTransaction
+import com.toshi.model.sofa.Payment
+import com.toshi.model.sofa.PaymentRequest
+import com.toshi.view.fragment.DialogFragment.PaymentConfirmationDialog
+
+class ResendHandler(private val activity: AppCompatActivity) {
+
+    fun showResendPaymentConfirmationDialog(receiver: User,
+                                            payment: Payment,
+                                            listener: (UnsignedTransaction) -> Unit) {
+        val dialog = PaymentConfirmationDialog.newInstanceToshiPayment(
+                receiver.toshiId,
+                payment.value, null,
+                PaymentType.TYPE_SEND
+        )
+
+        dialog.show(this.activity.supportFragmentManager, PaymentConfirmationDialog.TAG)
+        dialog.setOnPaymentConfirmationApprovedListener { _, unsignedTransaction -> listener(unsignedTransaction) }
+    }
+
+    fun showPaymentConfirmationDialog(receiver: User,
+                                      amount: String,
+                                      listener: (UnsignedTransaction) -> Unit) {
+        val dialog = PaymentConfirmationDialog.newInstanceToshiPayment(
+                receiver.toshiId,
+                amount, null,
+                PaymentType.TYPE_SEND
+        )
+        dialog.show(this.activity.supportFragmentManager, PaymentConfirmationDialog.TAG)
+        dialog.setOnPaymentConfirmationApprovedListener { _, unsignedTransaction -> listener(unsignedTransaction) }
+    }
+
+    fun showPaymentRequestConfirmationDialog(receiver: User,
+                                             paymentRequest: PaymentRequest,
+                                             listener: (UnsignedTransaction) -> Unit) {
+        val dialog = PaymentConfirmationDialog.newInstanceToshiPayment(
+                receiver.toshiId,
+                paymentRequest.value, null,
+                PaymentType.TYPE_REQUEST
+        )
+        dialog.show(this.activity.supportFragmentManager, PaymentConfirmationDialog.TAG)
+        dialog.setOnPaymentConfirmationApprovedListener { _, unsignedTransaction -> listener(unsignedTransaction) }
+    }
+
+    fun showResendDialog(listener: () -> Unit,
+                         deleteListener: () -> Unit) {
+        val resendDialog = BottomSheetDialog(this.activity)
+        val sheetView = this.activity.layoutInflater.inflate(R.layout.view_chat_resend, null)
+        val deleteView = sheetView.findViewById<LinearLayout>(R.id.deleteMessage)
+        deleteView.setOnClickListener {
+            resendDialog.dismiss()
+            deleteListener()
+        }
+        val retryView = sheetView.findViewById<LinearLayout>(R.id.retry)
+        retryView.setOnClickListener {
+            resendDialog.dismiss()
+            listener()
+        }
+        resendDialog.setContentView(sheetView)
+        resendDialog.show()
+    }
+}
