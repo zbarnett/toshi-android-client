b428011f8a538af55ac90a5cd2e342fa19eeb658
martintoften <martin@bakkenbaeck.no>
2018-04-10 18:00:24 +0200

    Pass wallet to DappManager

--- app/src/main/java/com/toshi/manager/DappManager.kt
+++ app/src/main/java/com/toshi/manager/DappManager.kt
@@ -17,53 +17,96 @@
 
 package com.toshi.manager
 
+import android.content.Context
+import com.toshi.crypto.HDWallet
 import com.toshi.manager.network.DirectoryInterface
 import com.toshi.manager.network.DirectoryService
 import com.toshi.model.network.dapp.DappResult
 import com.toshi.model.network.dapp.DappSearchResult
 import com.toshi.model.network.dapp.DappSections
+import com.toshi.util.dappUtil.CoinbaseDappVerifier
+import com.toshi.view.BaseApplication
 import rx.Scheduler
 import rx.Single
 import rx.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class DappManager(
         private val directoryService: DirectoryInterface = DirectoryService.get(),
+        context: Context = BaseApplication.get(),
+        private val coinbaseDappVerifier: CoinbaseDappVerifier = CoinbaseDappVerifier(context),
         private val scheduler: Scheduler = Schedulers.io()
 ) {
+
+    private var wallet: HDWallet? = null
+
+    fun init(wallet: HDWallet) {
+        this.wallet = wallet
+    }
 
     fun getFrontPageDapps(): Single<DappSections> {
         return directoryService
                 .getFrontpageDapps()
+                .flatMap { verifyCoinbaseDapp(it) }
                 .subscribeOn(scheduler)
+    }
+
+    private fun verifyCoinbaseDapp(dappSections: DappSections): Single<DappSections> {
+        return getWallet()
+                .map {
+                    coinbaseDappVerifier.updateUrlIfValid(dappSections, it.paymentAddress)
+                    dappSections
+                }
     }
 
     fun search(input: String): Single<DappSearchResult> {
         return directoryService
                 .search(input)
+                .flatMap { verifyCoinbaseDapp(it) }
                 .subscribeOn(scheduler)
     }
 
     fun getAllDapps(): Single<DappSearchResult> {
         return directoryService
                 .getAllDapps()
+                .flatMap { verifyCoinbaseDapp(it) }
                 .subscribeOn(scheduler)
     }
 
     fun getAllDappsWithOffset(offset: Int): Single<DappSearchResult> {
         return directoryService
                 .getAllDappsWithOffset(offset, 20)
+                .flatMap { verifyCoinbaseDapp(it) }
                 .subscribeOn(scheduler)
     }
 
     fun getAllDappsInCategoryWithOffset(categoryId: Int, offset: Int): Single<DappSearchResult> {
         return directoryService
                 .getAllDappsInCategory(categoryId, offset, 20)
+                .flatMap { verifyCoinbaseDapp(it) }
                 .subscribeOn(scheduler)
+    }
+
+    private fun verifyCoinbaseDapp(searchResult: DappSearchResult): Single<DappSearchResult> {
+        return getWallet()
+                .map {
+                    coinbaseDappVerifier.updateUrlIfValid(searchResult, it.paymentAddress)
+                    searchResult
+                }
     }
 
     fun getDapp(dappId: Long): Single<DappResult> {
         return directoryService
                 .getDapp(dappId)
                 .subscribeOn(scheduler)
+    }
+
+    private fun getWallet(): Single<HDWallet> {
+        return Single.fromCallable {
+            while (wallet == null) Thread.sleep(100)
+            return@fromCallable wallet ?: throw IllegalStateException("Wallet is null")
+        }
+        .subscribeOn(scheduler)
+        .timeout(20, TimeUnit.SECONDS)
     }
 }
