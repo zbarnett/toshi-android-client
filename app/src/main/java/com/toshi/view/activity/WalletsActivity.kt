b99ddb5b7a31133163470bae42735cdb650c083d
martintoften <martin@bakkenbaeck.no>
2018-04-26 10:59:48 +0200

    Add WalletsActivity

---
+++ app/src/main/java/com/toshi/view/activity/WalletsActivity.kt
@@ -0,0 +1,35 @@
+/*
+ * 	Copyright (c) 2017. Toshi Inc
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.toshi.view.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.toshi.R
+
+class WalletsActivity : AppCompatActivity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallets)
+        init()
+    }
+
+    private fun init() {
+
+    }
+}
