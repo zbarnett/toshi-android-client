242f1b7209fc075a60c0c69e177c3bdc731852a8
martintoften <martin@bakkenbaeck.no>
2018-05-03 11:33:49 +0200

    Update wallet layout
    Add avatar for wallet(random atm), render wallet name and change the button from "copy" to "receive"

--- app/src/main/java/com/toshi/view/fragment/toplevel/WalletFragment.kt
+++ app/src/main/java/com/toshi/view/fragment/toplevel/WalletFragment.kt
@@ -19,9 +19,6 @@ package com.toshi.view.fragment.toplevel
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.view.LayoutInflater
@@ -30,16 +27,12 @@ import android.view.ViewGroup
 import com.toshi.R
 import com.toshi.extensions.getColorById
 import com.toshi.extensions.toast
+import com.toshi.util.ImageUtil
 import com.toshi.view.adapter.WalletPagerAdapter
 import com.toshi.view.fragment.RefreshFragment
 import com.toshi.view.fragment.dialogFragment.ShareWalletAddressDialog
 import com.toshi.viewModel.WalletViewModel
-import kotlinx.android.synthetic.main.fragment_wallet.copy
-import kotlinx.android.synthetic.main.fragment_wallet.refreshLayout
-import kotlinx.android.synthetic.main.fragment_wallet.tabLayout
-import kotlinx.android.synthetic.main.fragment_wallet.viewPager
-import kotlinx.android.synthetic.main.fragment_wallet.walletAddress
-import kotlinx.android.synthetic.main.fragment_wallet.walletWrapper
+import kotlinx.android.synthetic.main.fragment_wallet.*
 
 class WalletFragment : TopLevelFragment() {
     companion object {
@@ -79,21 +72,12 @@ class WalletFragment : TopLevelFragment() {
     private fun initNetworkView() = getMainActivity()?.showNetworkStatusView()
 
     private fun initClickListeners() {
-        copy.setOnClickListener { handleCopyToClipboardClicked() }
-        walletWrapper.setOnClickListener { showShareWalletDialog() }
+        receive.setOnClickListener { showShareWalletDialog() }
     }
 
     private fun showShareWalletDialog() {
         val dialog = ShareWalletAddressDialog.newInstance()
         dialog.show(activity?.supportFragmentManager, ShareWalletAddressDialog.TAG)
-    }
-
-    private fun handleCopyToClipboardClicked() {
-        val walletAddress = viewModel.walletAddress.value ?: return
-        val clipboard = activity?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
-        val clip = ClipData.newPlainText(getString(R.string.payment_address), walletAddress)
-        clipboard?.primaryClip = clip
-        toast(R.string.copied_to_clipboard)
     }
 
     private fun initAdapter() {
@@ -119,11 +103,20 @@ class WalletFragment : TopLevelFragment() {
 
     private fun initObservers() {
         viewModel.walletAddress.observe(this, Observer {
-            if (it != null) walletAddress.setCollapsedText(it)
+            if (it != null) handleWalletAddress(it)
         })
         viewModel.error.observe(this, Observer {
             if (it != null) toast(it)
         })
+    }
+
+    private fun handleWalletAddress(paymentAddress: String) {
+        walletAddress.setCollapsedText(paymentAddress)
+        setWalletAvatar(paymentAddress)
+    }
+
+    private fun setWalletAvatar(paymentAddress: String) {
+        ImageUtil.loadIdenticon(paymentAddress, avatar)
     }
 
     fun stopRefreshing() {
