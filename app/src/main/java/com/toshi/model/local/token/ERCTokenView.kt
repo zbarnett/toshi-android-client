aba74a7a65b35352dbcc770e993bd37f0ee1aad6
martintoften <martin@bakkenbaeck.no>
2018-05-16 12:54:50 +0200

    Divide ERCToken into two classes
    One for the presentation layer and one for the data layeOne for the presentation layer and one for the data layer

---
+++ app/src/main/java/com/toshi/model/local/token/ERCTokenView.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2017. Toshi Inc
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.toshi.model.local.token
+
+import android.content.Intent
+import com.squareup.moshi.Json
+import com.toshi.model.network.token.ERC20Token
+import com.toshi.view.activity.ViewTokenActivity
+
+data class ERCTokenView(
+        val symbol: String?,
+        val name: String?,
+        val balance: String?,
+        val decimals: Int?,
+        @Json(name = "contract_address")
+        val contractAddress: String?,
+        val icon: String?
+) : Token() {
+
+    companion object {
+        private const val SYMBOL = "symbol"
+        private const val NAME = "name"
+        private const val DECIMALS = "decimals"
+        private const val BALANCE = "value"
+        private const val CONTRACT_ADDRESS = "contract_address"
+        private const val ICON = "icon"
+
+        fun buildIntent(intent: Intent, ERCTokenView: ERCTokenView): Intent {
+            return intent.apply {
+                putExtra(SYMBOL, ERCTokenView.symbol)
+                putExtra(NAME, ERCTokenView.name)
+                putExtra(BALANCE, ERCTokenView.balance)
+                putExtra(DECIMALS, ERCTokenView.decimals)
+                putExtra(CONTRACT_ADDRESS, ERCTokenView.contractAddress)
+                putExtra(ICON, ERCTokenView.icon)
+                putExtra(ViewTokenActivity.TOKEN_TYPE, ViewTokenActivity.ERC20_TOKEN)
+            }
+        }
+
+        fun getTokenFromIntent(intent: Intent): ERCTokenView? {
+            if (!hasAllExtras(intent)) return null
+            return ERCTokenView(
+                    intent.getStringExtra(SYMBOL),
+                    intent.getStringExtra(NAME),
+                    intent.getStringExtra(BALANCE),
+                    intent.getIntExtra(DECIMALS, 0),
+                    intent.getStringExtra(CONTRACT_ADDRESS),
+                    intent.getStringExtra(ICON)
+            )
+        }
+
+        private fun hasAllExtras(intent: Intent): Boolean {
+            return intent.hasExtra(SYMBOL) && intent.hasExtra(NAME)
+                    && intent.hasExtra(DECIMALS) && intent.hasExtra(BALANCE)
+                    && intent.hasExtra(CONTRACT_ADDRESS) && intent.hasExtra(ICON)
+        }
+
+        fun map(ERCToken: ERC20Token): ERCTokenView {
+            return ERCTokenView(
+                    symbol = ERCToken.symbol,
+                    name = ERCToken.name,
+                    balance = ERCToken.balance,
+                    decimals = ERCToken.decimals,
+                    contractAddress = ERCToken.contractAddress,
+                    icon = ERCToken.icon
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/toshi/model/network/token/ERC20Token.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017. Toshi Inc
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.toshi.model.network.token
+
+import com.squareup.moshi.Json
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class ERC20Token : RealmObject() {
+
+    @PrimaryKey
+    var primaryKey: String? = null
+    var walletIndex: Int? = null
+    var networkId: String? = null
+    @Json(name = "contract_address")
+    var contractAddress: String? = null
+    var symbol: String? = null
+    var name: String? = null
+    var balance: String? = null
+    var decimals: Int? = null
+    var icon: String? = null
+    var cacheTimestamp: Long = System.currentTimeMillis()
+
+    companion object {
+        private const val CACHE_TIMEOUT = 1000 * 60 * 5
+    }
+
+    fun setPrimaryKey(contractAddress: String, walletIndex: Int, networkId: String) {
+        primaryKey = "$contractAddress/$walletIndex/$networkId"
+    }
+
+    fun needsRefresh(): Boolean = System.currentTimeMillis() - cacheTimestamp > CACHE_TIMEOUT
+}
